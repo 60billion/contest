e1c02950e8d6170c31ccaeed947696eff77156dc
Allreview <60billion@allrvw.com>
2018-11-22 15:25:19 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyparser = require('body-parser');
+
+var app = express();
+app.use(bodyparser.json());
+
+var AWS = require('aws-sdk');
+AWS.config.region = 'ap-northeast-2';
+
+app.get('/',function(req,res){
+    res.send('Hello World');
+});
+
+app.listen(9000,function(){
+    console.log("connected server-port 9000");
+});
