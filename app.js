7396ec58c036a1255bc9db4fedab158e31f468ae
Allreview <60billion@allrvw.com>
2018-11-22 15:54:44 +0900

    connect server4

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var bodyparser = require('body-parser');
 var app = express();
 app.use(bodyparser.json());
 var router = require('./router/main')(app);
-
+app.use(router);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
