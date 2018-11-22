bdb43ab6c6049ec3e4873fdb1ad5c28fc02a6bfb
Allreview <60billion@allrvw.com>
2018-11-22 16:00:33 +0900

    connect server8

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ var bodyparser = require('body-parser');
 
 var app = express();
 app.use(bodyparser.json());
-var router = require('./router/main')(app);
-app.use('/main',router);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
@@ -12,6 +10,8 @@ app.engine('html', require('ejs').renderFile);
 var AWS = require('aws-sdk');
 AWS.config.region = 'ap-northeast-2';
 
+var main = require('./router/main')(app);
+app.use('/main',main);
 
 app.listen(9000,function(){
     console.log("connected server-port 9000");
