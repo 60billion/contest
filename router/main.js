90272aa588a84b097d65530f586af15d5c325597
Allreview <60billion@allrvw.com>
2018-11-22 15:57:22 +0900

    connect server6

--- router/main.js
+++ router/main.js
@@ -3,6 +3,9 @@ module.exports = function(app)
     var express = require('express');
     var bodyparser = require('body-parser');
     app.use(bodyparser.json());
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.engine('html', require('ejs').renderFile);
 
     var router = express.Router();
     router.get('/',function(req,res){
