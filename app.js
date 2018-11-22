ff0c6238ed03544bc888b37a367b7f1bf62a14d8
Allreview <60billion@allrvw.com>
2018-11-22 15:38:16 +0900

    connect server

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'ap-northeast-2';
 
 app.get('/',function(req,res){
-    res.send('Hello World');
+    res.send('Hello World, It will be contest website.');
 });
 
 app.listen(9000,function(){
